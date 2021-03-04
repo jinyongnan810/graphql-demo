e1999fa3d580dd66ed8eeb1fc0ad0f114d80f2ee
Yuunan Kin <yuunan.kin@gmail.com>
2021-03-05 08:59:10 +0900

    add user mutation

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -7,6 +7,7 @@ const {
   GraphQLInt,
   GraphQLSchema,
   GraphQLList,
+  GraphQLNonNull,
 } = grapghql;
 
 // const users = [
@@ -74,7 +75,26 @@ const RootQuery = new GraphQLObjectType({
     },
   },
 });
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+        companyId: { type: GraphQLString },
+      },
+      resolve: (parentValue, args) => {
+        return axios
+          .post(`http://localhost:3000/users/`, args)
+          .then((res) => res.data);
+      },
+    },
+  },
+});
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation,
 });
